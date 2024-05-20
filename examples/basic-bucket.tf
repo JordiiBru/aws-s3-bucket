c5dc250b1247834a5196a9b19ceea9fcc0eb5286
jordi.bru <jordi.bru@mango.com>
2024-05-20 15:47:40 +0200

    add: example added

--- examples/basic-bucket.tf
+++ examples/basic-bucket.tf
@@ -2,14 +2,13 @@ module "s3_bucket" {
   source = "../"
 
   # Required variables
-  stage           = "test"
-  owner           = "wanda"
-  purpose         = "tfg"
-
+  stage   = "test"
+  owner   = "wanda"
+  purpose = "tfg"
 
   # Optional variables
-  versioning      = true
-  public          = true
-  force_destroy   = false
-  static_website  = true
+  versioning     = true
+  public         = true
+  force_destroy  = false
+  static_website = true
 }
