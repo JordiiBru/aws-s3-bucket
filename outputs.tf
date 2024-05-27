ac715d0924c1625c1e4cacda163d993abd0bddb1
jordi.bru <jordi.bru@mango.com>
2024-05-27 19:53:41 +0200

    improved outputs file

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,26 @@
 # More on: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket#attribute-reference
 
 output "bucket_name" {
-  value = aws_s3_bucket.main_bucket.bucket
+  description = "The name of the main S3 bucket."
+  value       = aws_s3_bucket.main_bucket.bucket
 }
 
 output "bucket_arn" {
-  value = aws_s3_bucket.main_bucket.arn
+  description = "The ARN of the main S3 bucket."
+  value       = aws_s3_bucket.main_bucket.arn
 }
 
 output "zone_id" {
-  value = aws_s3_bucket.main_bucket.hosted_zone_id
+  description = "The hosted zone ID of the main S3 bucket."
+  value       = aws_s3_bucket.main_bucket.hosted_zone_id
 }
 
 output "regional_domain" {
-  value = aws_s3_bucket.main_bucket.bucket_regional_domain_name
+  description = "The regional domain name of the main S3 bucket."
+  value       = aws_s3_bucket.main_bucket.bucket_regional_domain_name
 }
 
 output "base_domain" {
-  value = aws_s3_bucket.main_bucket.bucket_domain_name
+  description = "The base domain name of the main S3 bucket."
+  value       = aws_s3_bucket.main_bucket.bucket_domain_name
 }
