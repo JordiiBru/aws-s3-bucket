aade34490068385044c227e5ec73c8c50229c96e
jordi.bru <jordi.bru@mango.com>
2024-06-14 23:07:08 +0200

    fix: dependency in policy fix

--- examples/basic-bucket.tf
+++ examples/basic-bucket.tf
@@ -4,11 +4,11 @@ module "s3_bucket" {
   # Comomon variables
   stage   = "test"
   owner   = "wanda"
-  purpose = "tfg"
+  purpose = "lele"
 
   # Custom variables
   versioning     = false
-  public         = false
-  force_destroy  = false
-  static_website = false
+  public         = true
+  force_destroy  = true
+  static_website = true
 }
--- main.tf
+++ main.tf
@@ -33,6 +33,8 @@ resource "aws_s3_bucket_policy" "bucket_policy_for_static_website" {
 
   bucket = aws_s3_bucket.main_bucket.id
   policy = data.aws_iam_policy_document.default_permissions.json
+
+  depends_on = [ aws_s3_bucket_public_access_block.bucket_access_block ]
 }
 
 data "aws_iam_policy_document" "default_permissions" {
